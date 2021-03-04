065f76f475a54519bd3d212d74e85e0b3badc28f
Kiril Kirilov <kirilkirilov@abv.bg>
2021-03-04 22:25:20 +0200

    Add files via upload

---
+++ Task01_Class_Box_Data/Box.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task01_Class_Box_Data
+{
+    public class Box
+    {
+        private double length;
+
+        private double width;
+        
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+        
+        public double Length
+        {
+            get
+            {
+                return this.length;
+            }
+            private set
+            {
+                this.ThrowIfInvalidArgument(value, nameof(Length));
+
+                this.length = value;
+            }
+        }
+
+        
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+            private set
+            {
+                this.ThrowIfInvalidArgument(value, nameof(Width));
+
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            private set
+            {
+                this.ThrowIfInvalidArgument(value, nameof(Height));
+
+                this.height = value;
+            }
+        }
+
+        //Surface Area = 2lw + 2lh + 2wh
+
+        public double SurfaceArea()
+        {
+            double result = 2 * length * width + 2 * length * height + 2 * width * height;
+            return result;
+        }
+
+        //Lateral Surface Area = 2lh + 2wh
+
+        public double LateralSurfaceArea()
+        {
+            double result = 2 * length * height + 2 * width * height;
+            return result;
+        }
+
+        
+        public double Volume()
+        {
+            double result = length * width * height;
+            return result;
+        }
+
+        private void ThrowIfInvalidArgument(double value, string side)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"{side} cannot be zero or negative.");
+            }
+        }
+
+    }
+}
---
+++ Task01_Class_Box_Data/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task01_Class_Box_Data
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // length, width and height
+
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box curentBox = new Box(length, width, height);
+                
+                Console.WriteLine($"Surface Area - {curentBox.SurfaceArea():f2}");
+
+                Console.WriteLine($"Lateral Surface Area - {curentBox.LateralSurfaceArea():f2}");
+
+                Console.WriteLine($"Volume - {curentBox.Volume():f2}");
+            }
+            catch (ArgumentException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
