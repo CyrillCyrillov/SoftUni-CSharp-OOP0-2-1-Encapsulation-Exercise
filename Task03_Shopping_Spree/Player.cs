dd7975b4600657f427f1991493a41e8e9bdd420c
Kiril Kirilov <kirilkirilov@abv.bg>
2021-03-08 23:21:52 +0200

    Add files via upload

---
+++ Task03_Shopping_Spree/Player.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task05_Football_Team_Generator
+{
+    public class Player
+    {
+        
+        private string name;
+        private int endurance;
+        private int sprint;
+        private int dribble;
+        private int passing;
+        private int shooting;
+        
+
+        public Player(string name, int endurance, int sprint, int dribble, int passing, int shooting)
+        {
+            Name = name;
+            Endurance =  endurance;
+            Sprint = sprint;
+            Dribble = dribble;
+            Passing = passing;
+            Shooting = shooting;
+    }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("A name should not be empty.");
+                }
+
+                name = value;
+            }
+        }
+
+        public double Stats
+        {
+            get
+            {
+                return Math.Round((dribble + endurance + passing + shooting + sprint) / 5.0);
+            }
+        }
+
+        private int Endurance
+        {
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentException("Endurance should be between 0 and 100.");
+                }
+
+                endurance = value;
+            }
+        }
+
+        private int Sprint
+        {
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentException("Sprint should be between 0 and 100.");
+                }
+
+                sprint = value;
+            }
+        }
+
+        private int Dribble
+        {
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentException("Dribble should be between 0 and 100.");
+                }
+
+                dribble = value;
+            }
+        }
+
+        private int Passing
+        {
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentException("Passing should be between 0 and 100.");
+                }
+
+                passing = value;
+            }
+        }
+
+        private int Shooting
+        {
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentException("Shooting should be between 0 and 100.");
+                }
+
+                shooting = value;
+            }
+        }
+    }
+}
--- Task03_Shopping_Spree/Program.cs
+++ Task03_Shopping_Spree/Program.cs
@@ -1,58 +1,76 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace Task03_Shopping_Spree
+namespace Task05_Football_Team_Generator
 {
     public class Program
     {
         static void Main(string[] args)
         {
             
+            List<Team> allTeams = new List<Team>();
 
-            string[] dataLinePersons = Console.ReadLine().Split(new char[] { '=', ';' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string nextComand = string.Empty;
+            while ((nextComand = Console.ReadLine()) != "END")
+            {
+                string[] datas = nextComand.Split(';', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                try
+                {
+                    //datas[0] -> Type of the comand
+                    switch (datas[0])
+                    {
+                        case "Team":
+                            //datas[1] -> Name of the new team
+                            allTeams.Add(new Team(datas[1]));
+                            break;
+
+                        case "Add":
+                            //datas[1] -> Name of the team, datas[2] -> Name of the player, datas[3 ... 7] - skil levels
+                            Team teamToAddPlayer = allTeams.FirstOrDefault(n => n.Name == datas[1]);
+
+                            if (teamToAddPlayer == null)
+                            {
+                                throw new ArgumentException($"Team {datas[1]} does not exist.");
+                            }
+                            else
+                            {
+                                teamToAddPlayer.AddPlayer(new Player(datas[2], int.Parse(datas[3]), int.Parse(datas[4]), int.Parse(datas[5]), int.Parse(datas[6]), int.Parse(datas[7])));
+                            }
+                            break;
+
+                        case "Remove":
+                            //datas[1] -> Name of the team, datas[2] -> Name of the player
+                            Team teamToRemovePlayer = allTeams.FirstOrDefault(n => n.Name == datas[1]);
+                            teamToRemovePlayer.RemovePlayer(datas[2]);
+                            break;
+
+                        case "Rating":
+                            //datas[1] -> Name of the team
+                            Team teamToRating = allTeams.FirstOrDefault(n => n.Name == datas[1]);
+                            if (teamToRating == null)
+                            {
+                                throw new ArgumentException($"Team {datas[1]} does not exist.");
+                            }
+                            else
+                            {
+                                Console.WriteLine(teamToRating);
+                            }
+                            break;
+                        
+                        default:
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
             
-            string[] dataLineProducts = Console.ReadLine().Split(new char[] { '=', ';' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
-
-            try
-            {
-                Dictionary<string, Person> allPersons = new Dictionary<string, Person>();
-                
-                for (int i = 0; i <= dataLinePersons.Length-2; i += 2)
-                {
-                    Person nextPerson = new Person(dataLinePersons[i], double.Parse(dataLinePersons[i + 1]));
-                    allPersons.Add(dataLinePersons[i], nextPerson);
-                }
-
-                Dictionary<string, Product> allProducts = new Dictionary<string, Product>();
-
-                for (int i = 0; i <= dataLineProducts.Length - 2; i += 2)
-                {
-                    Product nextProduct = new Product(dataLineProducts[i], double.Parse(dataLineProducts[i + 1]));
-                    allProducts.Add(dataLineProducts[i], nextProduct);
-                }
-
-                string nextComand;
-                while ((nextComand = Console.ReadLine()) != "END")
-                {
-                    string[] curentData = nextComand.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
-
-                    Person curentPerson = allPersons[curentData[0]];
-                    Product curentProduct = allProducts[curentData[1]];
-
-                    curentPerson.Bought(curentProduct);
-                }
-
-                foreach (var element in allPersons.Values)
-                {
-                    Console.WriteLine(element);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
+            
+            
             //Console.WriteLine("Hello World!");
         }
     }
